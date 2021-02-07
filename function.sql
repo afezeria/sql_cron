fe34a01bbdeacd88419a6238d7e8b9b57f1cf584
zodal <zodal@outlook.com>
2021-02-07 11:26:59 +0800

    fix bug, allow multiple symbols in a field

--- function.sql
+++ function.sql
@@ -1,3 +1,4 @@
+set search_path = 'public';
 -- author afezeria
 --  文件格式說明
 --  ┌──分鐘（0 - 59）
@@ -8,18 +9,17 @@
 --  │ │ │ │ │
 --  *  *  *  *  *
 -- 支持的符号： , - / ?(日和星期)
--- 不支持同时出现多个符号
 create or replace function get_next_execution_time(cron text, last_exec_time timestamptz) returns timestamptz
     language plpgsql
     immutable as
 $$
 declare
-    cron_item      text[]      := regexp_split_to_array(cron, '\s+');
+    cron_item      text[] := regexp_split_to_array(cron, '\s+');
 --     上一次
-    l_month        int         := extract(month from last_exec_time);
-    l_day          int         := extract(day from last_exec_time);
-    l_hour         int         := extract(hour from last_exec_time);
-    l_minute       int         := extract(minute from last_exec_time);
+    l_month        int    := extract(month from last_exec_time);
+    l_day          int    := extract(day from last_exec_time);
+    l_hour         int    := extract(hour from last_exec_time);
+    l_minute       int    := extract(minute from last_exec_time);
 --     可用的
     a_week_day_arr int[];
     a_month_arr    int[];
@@ -37,7 +37,7 @@ declare
     n_hour         int;
 --     0-59
     n_minute       int;
-    n_year         int         := extract(year from last_exec_time);
+    n_year         int    := extract(year from last_exec_time);
 --     表达式
     expr_dow       text;
     expr_month     text;
@@ -47,7 +47,7 @@ declare
     n_timestamptz  timestamptz;
 --     为true表示忽略dayOfWeek(不判断dayOfWeek）,为false表示忽略day（此时day下界永远为1）
     ignore_dow     bool;
-    loop_count     int         := 0;
+    loop_count     int    := 0;
 begin
     if array_length(cron_item, 1) <> 5 then
         raise exception 'invalid cron expression, expression can only 5 item';
@@ -70,17 +70,19 @@ begin
     <<l1>>
     loop
         loop_count = loop_count + 1;
-        if loop_count > 100 then
-            raise exception '无法找到下一次执行时间';
+        if loop_count > 300 then
+            raise exception 'cycle-index greater than 300, cannot find next time, exit function';
         end if;
         a_week_day_arr = filter_arr_in_range(
-                parse_cron_sub_expr_and_get_range('day_of_week', expr_dow, 0, 6),
+                parse_cron_sub_expr_and_get_range('day_of_week', expr_dow, null, 0, 6),
                 null, null
             );
+        -- raise notice 'week %',a_week_day_arr;
         a_month_arr = filter_arr_in_range(
-                parse_cron_sub_expr_and_get_range('month', expr_month, 1, 12),
+                parse_cron_sub_expr_and_get_range('month', expr_month, null, 1, 12),
                 l_month, null
             );
+        -- raise notice 'month %',a_month_arr;
         if array_length(a_month_arr, 1) is null then
 --         没有可用月份时年分加一，
             n_year = n_year + 1;
@@ -93,12 +95,13 @@ begin
             continue;
         end if;
         a_day_arr = filter_arr_in_range(
-                parse_cron_sub_expr_and_get_range('day', expr_day, 1, 31),
+                parse_cron_sub_expr_and_get_range('day', expr_day, null, 1, 31),
                 l_day,
 --             取当前月份
                 extract(days FROM date_trunc('month', make_date(n_year, a_month_arr[0], 1)) +
                                   interval '1 month - 1 day')::int
             );
+        -- raise notice 'day %',a_day_arr;
         if array_length(a_day_arr, 1) is null then
             l_month = l_month + 1;
             l_day = 1;
@@ -108,9 +111,10 @@ begin
         end if;
 
         a_hour_arr = filter_arr_in_range(
-                parse_cron_sub_expr_and_get_range('hour', expr_hour, 0, 23),
+                parse_cron_sub_expr_and_get_range('hour', expr_hour, null, 0, 23),
                 l_hour, null
             );
+        -- raise notice 'hour %',a_hour_arr;
         if array_length(a_hour_arr, 1) is null then
             l_day = l_day + 1;
             l_hour = 0;
@@ -118,9 +122,10 @@ begin
             continue;
         end if;
         a_minute_arr = filter_arr_in_range(
-                parse_cron_sub_expr_and_get_range('minute', expr_minute, 0, 59),
+                parse_cron_sub_expr_and_get_range('minute', expr_minute, null, 0, 59),
                 l_minute, null
             );
+        -- raise notice 'minute %',a_minute_arr;
         if array_length(a_minute_arr, 1) is null then
             l_hour = l_hour + 1;
             l_minute = 0;
@@ -149,6 +154,8 @@ begin
                     return n_timestamptz;
                 end if;
             end loop;
+        l_hour = l_hour + 1;
+        l_minute = 0;
     end loop;
 end;
 $$;
@@ -181,55 +188,65 @@ $$;
 comment on function filter_arr_in_range(int[], int, int) is '根据区间上界和下界过滤int数组，上界和下界为空';
 
 
-create or replace function parse_cron_sub_expr_and_get_range(d_name text, expr text, lp int, rp int) returns int[]
+create or replace function parse_cron_sub_expr_and_get_range(d_name text, expr text, sub_expr text, lp int, rp int) returns int[]
     language plpgsql
     immutable as
 $$
 declare
     ia      int[];
---     range/enum/step
+--     range/step
     e_type  text;
     r_left  int;
     r_right int;
 begin
-    if regexp_match(expr,
-                    '^(\?|\*|\d{1,2}-\d{1,2}|\d{1,2}/\d{1,2}|\d{1,2}(,\d{1,2})?)$') is null then
-        raise exception 'invalid % group: %',d_name,expr;
+    if sub_expr is null then
+        if position(',' in expr) > 0 then
+            select array(select distinct unnest(i)
+                         from (
+                                  select parse_cron_sub_expr_and_get_range(d_name, expr, x, lp, rp) as i
+                                  from unnest(regexp_split_to_array(expr, ',')) as t(x)
+                              ) t)
+            into ia;
+            return ia;
+        else
+            sub_expr := expr;
+        end if;
     end if;
-    if length(expr) = 1 then
-        if expr = '*' then
+    if regexp_match(sub_expr,
+                    '^(\?|\*|\d{1,2}-\d{1,2}|\d{1,2}/\d{1,2}|\d{1,2}(,\d{1,2})?)$') is null then
+        raise exception 'invalid % group: %',d_name,sub_expr;
+    end if;
+    if length(sub_expr) = 1 then
+        if sub_expr = '*' then
             return ARRAY(SELECT * FROM generate_series(lp, rp));
-        elsif expr = '?' then
+        elsif sub_expr = '?' then
             if d_name != 'day' and d_name != 'day_of_week' then
                 raise exception 'only day and day_of_week group support ''?''';
             else
                 return ARRAY(SELECT * FROM generate_series(lp, rp));
             end if;
         else
-            r_left = expr::int;
+            r_left = sub_expr::int;
             if r_left < lp then
                 raise exception 'invalid % group, % number must be between % and %',d_name,d_name,lp,rp;
             end if;
-            return array [expr::int];
+            return array [sub_expr::int];
         end if;
-    elsif length(expr) = 2 then
-        r_left = expr::int;
+    elsif length(sub_expr) = 2 then
+        r_left = sub_expr::int;
         if r_left < lp or r_left > rp then
             raise exception 'invalid % group, % number must be between % and %',d_name,d_name,lp,rp;
         end if;
-        return array [expr::int];
+        return array [sub_expr::int];
     end if;
---     如果expr中包含 - 就是区间型，不包含是枚举型
-    if position('-' in expr) <> 0 then
+    if position('-' in sub_expr) <> 0 then
         e_type = 'range';
-    elsif position('/' in expr) <> 0 then
+    elsif position('/' in sub_expr) <> 0 then
         e_type = 'step';
-    else
-        e_type = 'enum';
     end if;
     if e_type = 'range' then
-        r_left = (regexp_split_to_array(expr, '-'))[1]::int;
-        r_right = (regexp_split_to_array(expr, '-'))[2]::int;
+        r_left = (regexp_split_to_array(sub_expr, '-'))[1]::int;
+        r_right = (regexp_split_to_array(sub_expr, '-'))[2]::int;
         if r_left < lp or r_left > rp or r_right < lp or r_right > rp then
             raise exception 'invalid % group, % number must be between % and %',d_name,d_name,lp,rp;
         end if;
@@ -239,13 +256,13 @@ begin
         ia = ARRAY(SELECT * FROM generate_series(r_left, r_right));
     elsif e_type = 'step' then
 --         起始值
-        r_left = (regexp_split_to_array(expr, '/'))[1]::int;
+        r_left = (regexp_split_to_array(sub_expr, '/'))[1]::int;
 --         步长
-        r_right = (regexp_split_to_array(expr, '/'))[2]::int;
+        r_right = (regexp_split_to_array(sub_expr, '/'))[2]::int;
         if r_left < lp or r_left > rp then
             raise exception 'invalid % group, start value must be between % and %',d_name,lp,rp;
         end if;
-        if r_right < 2 or r_right > rp then
+        if r_right < 1 or r_right > rp then
             raise exception 'invalid % group, step size must be between 2 and %',d_name,rp - 1;
         end if;
         ia = array [r_left];
@@ -254,15 +271,8 @@ begin
             exit when r_left > rp;
             ia = array_append(ia, r_left);
         end loop;
-    else
-        select array_agg(x)
-        into ia
-        from (SELECT unnest(regexp_split_to_array(expr, ','))::int AS x
-              ORDER BY x) a;
-        if ia[1] < lp or ia[array_upper(ia, 1)] > rp then
-            raise exception 'invalid % group, start value must be between % and %',d_name,lp,rp;
-        end if;
     end if;
     return ia;
 end;
 $$;
+
